dbe473337eb5dd19134afb42f67b44e18440d0eb
Olga Bezginova <bezginova.olga@gmail.com>
2018-07-31 20:57:08 +0300

    Paint perimeter cells

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@ function randomNumber(min, max) {
 	return min + Math.floor(Math.random() * (max + 1 - min));
 }
 
+/*Create table of td-elements*/
+
 let tdList = document.querySelectorAll("td");
+let trList = document.querySelectorAll("tr");
 
 let tdArray = [];
-
 let k = 0;
+
 for(let i = 0; i < 10; i++) {
     tdArray[i] = [];
     for(let j = 0; j < 10; j++) {
@@ -15,6 +18,28 @@ for(let i = 0; i < 10; i++) {
         k++;
     }
 }
+
+
+/*Paint perimeter cells*/
+
+for(let i = 0; i < trList.length; i++) {
+    let cells = trList[i].children;
+    console.log(cells);
+    if(!trList[i].previousElementSibling || !trList[i].nextElementSibling) {
+        for(let j = 0; j < cells.length; j++){
+            cells[j].className = 'perimeter';
+        }
+    }
+    else{
+        trList[i].lastElementChild.className  = 'perimeter';
+        trList[i].firstElementChild.className = 'perimeter'; 
+    }   
+}
+
+
+
+
+
 
 
 
