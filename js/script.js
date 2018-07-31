b829052f30eb7daa0c13c708d26ba5a0a0628491
Olga Bezginova <bezginova.olga@gmail.com>
2018-07-31 21:44:04 +0300

    Create table via js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,34 @@ function randomNumber(min, max) {
 	return min + Math.floor(Math.random() * (max + 1 - min));
 }
 
+
+/*Create table*/
+
 let tableSize = 10;
 
+let table = document.createElement('table');
+let tbody = document.createElement('tbody');
+let tr    = document.createElement('tr');
+let td    = document.createElement('td');
 
-/*Create table of td-elements*/
+document.body.appendChild(table);
+table.appendChild(tbody);
 
-let tdList = document.querySelectorAll("td");
-let trList = document.querySelectorAll("tr");
+for(let i = 0; i < tableSize; i++) {
+    let newTd = td.cloneNode(true);
+    tr.appendChild(newTd);
+}
+
+for(let i = 0; i < tableSize; i++) {
+    let newTr = tr.cloneNode(true);
+    tbody.appendChild(newTr);
+}
+
+
+/*Create matrix of td-elements*/
+
+let tdList = document.querySelectorAll('td');
+let trList = document.querySelectorAll('tr');
 
 let tdArray = [];
 let k = 0;
